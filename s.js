39c986acc2bd55512c67035991764d298feb53a2
huzaifa954 <ahmedniazi954@gmail.com>
2024-07-30 05:31:12 -0700

    add

---
+++ s.js
@@ -0,0 +1,15 @@
+// var gender = confirm("are you male")
+// var age = confirm("are you 18+");
+// // age = parseInt(age)
+// if (gender) {
+//     alert("allowed")
+// } else {
+//     alert("not allowed")
+// }
+// if (age) { 
+//     alert("allowed")
+// } else {
+//     alert("not allowed")
+// }
+ var students="ali,zeshan,ahmed"
+ console.log(students)
