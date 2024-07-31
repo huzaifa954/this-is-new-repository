f7c67911565a58c8655dc99eb6b512a7654380fe
huzaifa954 <ahmedniazi954@gmail.com>
2024-07-31 05:27:21 -0700

    add

--- s.js
+++ s.js
@@ -1,16 +1,22 @@
-// var gender = confirm("are you male")
-// var age = confirm("are you 18+");
-// // age = parseInt(age)
-// if (gender) {
-//     alert("allowed")
-// } else {
-//     alert("not allowed")
-// }
-// if (age) { 
-//     alert("allowed")
-// } else {
-//     alert("not allowed")
-// }
- var students="ali,zeshan,ahmed"
- console.log(students)
- students.unshift=("ali")
+// // var gender = confirm("are you male")
+// // var age = confirm("are you 18+");
+// // // age = parseInt(age)
+// // if (gender) {
+// //     alert("allowed")
+// // } else {
+// //     alert("not allowed")
+// // }
+// // if (age) { 
+// //     alert("allowed")
+// // } else {
+// //     alert("not allowed")
+// // }
+this
+//  var students=["ali","zeshan","ahmed"]
+//  console.log(students)
+//  console.log(students[0])
+//  console.log(students[1])
+//  console.log(students[2])
+
+//  // students.pop("ali");
+// //  console.log(students)
