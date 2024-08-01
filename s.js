38296726dbbb9e96ed7d25fdca19c91035a4493a
huzaifa954 <ahmedniazi954@gmail.com>
2024-08-01 06:05:12 -0700

    add

--- s.js
+++ s.js
@@ -11,12 +11,19 @@
 // // } else {
 // //     alert("not allowed")
 // // }
-this
-//  var students=["ali","zeshan","ahmed"]
-//  console.log(students)
-//  console.log(students[0])
-//  console.log(students[1])
-//  console.log(students[2])
-
-//  // students.pop("ali");
-// //  console.log(students)
+ 
+ var students=["ali","zeshan","ahmed"]
+ console.log(students)
+ console.log(students[0])
+ console.log(students[1])
+ console.log(students[2])
+console.log(students)
+  students.pop();
+  console.log(students)
+students.unshift("ali","zeeshan");
+console.log(students)
+students.splice(2,1);
+console.log(students)
+var array=("0","0","one","two","three")
+array.slice(one)
+console.log(array)
