ab30452592b2bffcf8d736598ef912e6c3710d7a
huzaifa954 <ahmedniazi954@gmail.com>
2024-08-02 08:17:19 -0700

    add new js

--- s.js
+++ s.js
@@ -1,29 +1,21 @@
-// // var gender = confirm("are you male")
-// // var age = confirm("are you 18+");
-// // // age = parseInt(age)
-// // if (gender) {
-// //     alert("allowed")
-// // } else {
-// //     alert("not allowed")
-// // }
-// // if (age) { 
-// //     alert("allowed")
-// // } else {
-// //     alert("not allowed")
-// // }
- 
- var students=["ali","zeshan","ahmed"]
- console.log(students)
- console.log(students[0])
- console.log(students[1])
- console.log(students[2])
+var students=["ali","zeeshan","ahmed"]
+students.push("qosain")
+students.unshift("li");
+//students.pop();
 console.log(students)
-  students.pop();
-  console.log(students)
-students.unshift("ali","zeeshan");
-console.log(students)
-students.splice(2,1);
-console.log(students)
-var array=("0","0","one","two","three")
-array.slice(one)
-console.log(array)
+// console.log(students)
+console.log(students[0])
+console.log(students[4])
+let h=(25)
+let u=(22)
+//if (condition) {
+    
+//} else {
+    
+    //}
+ if(h===u){
+    alert(true)
+}
+else{
+alert(false)
+}
